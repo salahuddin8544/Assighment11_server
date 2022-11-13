3b93081fcba6548391aa63afa1f2649cba3b8edf
Salahuddin Ahmed <salahuddinahmed8544@gmail.com>
2022-11-13 14:02:30 -0800

    start server site

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+require('dotenv').config()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
