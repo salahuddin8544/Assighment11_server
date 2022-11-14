694e6cc81aa4df5568fc90b7aa8f6c4434fcf7de
Salahuddin Ahmed <salahuddinahmed8544@gmail.com>
2022-11-13 22:45:17 -0800

    create allservices

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors())
@@ -24,6 +24,20 @@ async function run(){
             const query = {}
             const cursor = serviceCollection.find(query)
             const service = await cursor.limit(3).toArray()
+            res.send(service)
+        })
+
+        app.get('/services/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+           app.get('/allServices', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const service = await cursor.toArray()
             res.send(service)
         })
     }
